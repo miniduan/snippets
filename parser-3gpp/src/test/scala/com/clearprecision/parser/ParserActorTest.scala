f0f1aa0e4953f9711246cdfe7b87991765a49145
Tony Ayres <tayres@gmail.com>
2013-09-20 21:12:59 +0100

    Adding actor system shutdown in afterAll for ParserActorTest

--- parser-3gpp/src/test/scala/com/clearprecision/parser/ParserActorTest.scala
+++ parser-3gpp/src/test/scala/com/clearprecision/parser/ParserActorTest.scala
@@ -4,28 +4,32 @@ import org.junit.Assert.assertEquals
 import org.scalatest.WordSpec
 import org.scalatest.junit.AssertionsForJUnit
 import org.scalatest.matchers.MustMatchers
-
 import com.clearprecision.parser.messages.Parse
 import com.clearprecision.parser.messages.ParseResult
-
 import akka.actor.ActorSystem
 import akka.testkit.ImplicitSender
 import akka.testkit.TestActorRef
 import akka.testkit.TestKit
+import org.scalatest.BeforeAndAfterAll
 
 class ParserActorTest extends TestKit(ActorSystem("testActorSystem")) with ImplicitSender
   with WordSpec
-  with MustMatchers with AssertionsForJUnit {
+  with MustMatchers with AssertionsForJUnit with BeforeAndAfterAll {
+
+  override def afterAll {
+    println("Stop actor system")
+    TestKit.shutdownActorSystem(system)
+  }
 
   "A parser actor" must {
-    "successfully parse a file" in {     
+    "successfully parse a file" in {
       val actorRef = TestActorRef[ParserActor]
-      val testRequest = new Parse("src/test/resources/test-pm-file-1-3gpp.xml")     
+      val testRequest = new Parse("src/test/resources/test-pm-file-1-3gpp.xml")
       actorRef ! testRequest
       expectMsgClass(classOf[ParseResult])
     }
   }
-  
+
   "A parser actor" must {
     "return an error when an unknown object is received" in {
       val actorRef = TestActorRef[ParserActor]
