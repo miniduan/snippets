6b2239dfe115a359a808d445af7e3a2d3a14a434
Tony Ayres <tayres@gmail.com>
2013-09-22 21:28:17 +0100

    Updating data storage actor to log parsed object

--- parser-3gpp/src/main/scala/com/clearprecision/data/DataStorageActor.scala
+++ parser-3gpp/src/main/scala/com/clearprecision/data/DataStorageActor.scala
@@ -3,12 +3,19 @@ package com.clearprecision.data
 import akka.actor.Actor
 import com.clearprecision.parser.messages.ParseResult
 import akka.actor.ActorLogging
+import scala.collection.JavaConversions._
 
 class DataStorageActor extends Actor with ActorLogging{
 
   def receive = {
     case ParseResult(_, _, _, measurementData) => {
       log.debug(measurementData.toString())
+      log.debug(measurementData.getFileHeader.getVendorName)
+      log.debug(measurementData.getFileHeader.getDnPrefix)
+      
+      val data = measurementData.getMeasData
+      data.foreach(measData => log.debug("Measedata "+measData))
+            
     }
     case _ => log.error("Ignoring unkown data received by DataStorageActor")
   }
