330cefe6554383d0a2d219e12aeaabc6b92d5e3b
Tony Ayres <tayres@gmail.com>
2013-10-04 22:48:22 +0100

    Updating state machine transition

--- akka-sandbox/src/main/scala/com/clearprecision/akka/StateMachineActor.scala
+++ akka-sandbox/src/main/scala/com/clearprecision/akka/StateMachineActor.scala
@@ -2,6 +2,7 @@ package com.clearprecision.akka
 
 import akka.actor.Actor
 import akka.actor.FSM
+import scala.concurrent.duration._
 
 import com.clearprecision.akka.messages._
 
@@ -9,6 +10,9 @@ class StateMachineActor extends Actor with FSM[State, Data] {
 
   startWith(Stopped, RequestData)
 
+  when(Running, stateTimeout = 1 second) {
+    case _ => stay
+  }
 
   initialize()
 
