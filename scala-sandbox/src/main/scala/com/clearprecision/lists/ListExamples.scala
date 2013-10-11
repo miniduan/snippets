00043c57d7cc2d9900cc0e1b09611d07a91179e9
Tony Ayres <tayres@gmail.com>
2013-10-11 20:54:32 +0100

    Single and multi threaded list processing examples

--- scala-sandbox/src/main/scala/com/clearprecision/lists/ListExamples.scala
+++ scala-sandbox/src/main/scala/com/clearprecision/lists/ListExamples.scala
@@ -3,12 +3,32 @@ package com.clearprecision.lists
 object ListExamples extends App {
 
   val intList = List(1, 2, 3, 4, 5)
+  
+  val ints = for (i <-1 to 1000) yield i
+  
 
-  def listComprehension() = {      
+  def listComprehension() = {
     val newList = for (x <- intList; if x > 2) yield x
     newList
   }
+
+  def paralellList() = {
+    var start = System.currentTimeMillis()
+    val result = intList.map(el => el * el)
+    val stTime = System.currentTimeMillis()-start
+    
+    start = System.currentTimeMillis()
+    val parResul = intList.par.map(el => el * el)
+    val mtTime = System.currentTimeMillis()-start
+    (stTime, mtTime)
+    
+  }
+
+  print(listComprehension)
   
-  listComprehension
+  val res = paralellList
+  print("Single threaded operation took "+res._1)
+  print("Multi threaded threaded operation took "+res._2)
+  
 
 }
