30ab6faa1c72af831625cce6d1b8554a32844a4c
Tony Ayres <tayres@gmail.com>
2013-10-31 21:03:53 +0000

    Akka futures experiments. Confusing as hell

--- akka-sandbox/src/main/scala/com/clearprecision/akka/FutureProcessor.scala
+++ akka-sandbox/src/main/scala/com/clearprecision/akka/FutureProcessor.scala
@@ -1,13 +1,22 @@
 package com.clearprecision.akka
 
-import akka.actor.Actor
-import com.clearprecision.akka.messages.TaskRequest
+import scala.collection.immutable.List
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
+import java.util.concurrent.Executors
 
-class FutureProcessor extends Actor {
-
-  def receive = {
-    case TaskRequest(x) => {      
-    }
+class FutureProcessor extends Application {
+    
+  implicit val ec = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(2))
+  
+  val data = List(1,2,3,4)
+  val factors = List(10,20,30,40)
+  val compositeList = List(data, factors)
+  
+  val future = Future {    
+    val result = compositeList.foreach(list => println(list.sum))        
   }
   
+  future foreach println    
+
 }
